157bab07d2a747ee781ee7ee6037105a0819de3c
STACiA <69411527+staciax@users.noreply.github.com>
2025-02-04 00:18:06 +0700

    chore: add comments for ulid format integration in typebox

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
 import { app } from '@/app';
 import { env } from '@/core/config';
 
+// import { FormatRegistry } from '@sinclair/typebox';
+// Add 'ulid' format to typebox
+// https://github.com/colinhacks/zod/blob/e376cda8e14d3caa09bc2148ffc668748118db6b/src/types.ts#L638
+// const ulidRegex = /^[0-9A-HJKMNP-TV-Z]{26}$/i;
+// FormatRegistry.Set('ulid', (value) => ulidRegex.test(value));
+
 app.listen(
     {
         port: env.PORT,
