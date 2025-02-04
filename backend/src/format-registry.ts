4a0bc19677d7031ba48feb07526283414a5741b7
STACiA <69411527+staciax@users.noreply.github.com>
2025-02-04 15:33:47 +0700

    feat: add ULID format registration to typebox format registry

---
+++ backend/src/format-registry.ts
@@ -0,0 +1,8 @@
+import { FormatRegistry } from '@sinclair/typebox';
+
+// https://github.com/colinhacks/zod/blob/e376cda8e14d3caa09bc2148ffc668748118db6b/src/types.ts#L638
+const ulidRegex = /^[0-9A-HJKMNP-TV-Z]{26}$/i;
+
+export const registerUlidFormat = () => {
+    FormatRegistry.Set('ulid', (value) => ulidRegex.test(value));
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,8 @@
 import { app } from '@/app';
 import { env } from '@/core/config';
+import { registerUlidFormat } from '@/format-registry';
 
-// import { FormatRegistry } from '@sinclair/typebox';
-// Add 'ulid' format to typebox
-// https://github.com/colinhacks/zod/blob/e376cda8e14d3caa09bc2148ffc668748118db6b/src/types.ts#L638
-// const ulidRegex = /^[0-9A-HJKMNP-TV-Z]{26}$/i;
-// FormatRegistry.Set('ulid', (value) => ulidRegex.test(value));
+registerUlidFormat();
 
 app.listen(
     {
