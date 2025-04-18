65a1ee765463f7ba9c86821accf3e1534fb28cf2
STACiA <stacia.dev@gmail.com>
2025-04-18 22:53:18 +0700

    fix: update password verification algorithm from argon2d to argon2id

--- backend/src/core/security.ts
+++ backend/src/core/security.ts
@@ -21,7 +21,7 @@ export async function verifyPassword(
     plainPassword: string,
     hashedPassword: string,
 ) {
-    return await Bun.password.verify(plainPassword, hashedPassword, 'argon2d');
+    return await Bun.password.verify(plainPassword, hashedPassword, 'argon2id');
 }
 
 export async function getPasswordHash(password: string) {
